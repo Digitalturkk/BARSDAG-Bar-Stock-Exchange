db2da977ed82b07900d87bd4d785275b91f813c4
DigitalTurk <huseynovfarid1111@gmail.com>
2025-12-10 23:53:12 +0400

    added initial Java Spring Project

---
+++ BARSDAG-Java/src/main/java/org/DigitalDucks/BARSDAG/BarsdagApplication.java
@@ -0,0 +1,13 @@
+package org.DigitalDucks.BARSDAG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarsdagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarsdagApplication.class, args);
+	}
+
+}
---
+++ BARSDAG-Java/src/test/java/org/DigitalDucks/BARSDAG/BarsdagApplicationTests.java
@@ -0,0 +1,13 @@
+package org.DigitalDucks.BARSDAG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarsdagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
